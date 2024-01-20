b31607509e68f1b25abef919577a0049992a8779
rasleenkaur <rasleenbedi04@gmail.com>
2024-01-20 16:44:29 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,9 @@ app.use(express.json());
 ConnectDB();
 
 
-app.get("/", (req, res) => {
-    res.send("API is running");
-})
+// app.get("/", (req, res) => {
+//     res.send("API is running");
+// })
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "/frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
